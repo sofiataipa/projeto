ad00644c7342ecdc853992527a78996e99967d8c
sofiataipa <taipa.sofia@gmail.com>
2022-04-29 19:36:03 +0100

    Terrain + Char movement

---
+++ projeto2022/Assets/characters/hero/KeyboardControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControl : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("isRunning", Input.GetAxis("Vertical") > 0);
+        
+    }
+}
